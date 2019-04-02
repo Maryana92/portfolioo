9a82f3c571ed30d11da02fbf4f39e5d3f8b22a7b
Maryana <sentsiv.m@gmail.com>
2019-04-02 03:05:43 +0300

    Updates

---
+++ precache-manifest.4ebc667e6d1dadde7e598f739b190057.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "86816fcca6b92ac284f8",
+    "url": "/swapi/static/js/runtime~main.3fb9cbf6.js"
+  },
+  {
+    "revision": "8f948718793959f03372",
+    "url": "/swapi/static/js/main.3f5791a5.chunk.js"
+  },
+  {
+    "revision": "d9ab65a65cabcf4b1ce6",
+    "url": "/swapi/static/js/2.b2c8fbf7.chunk.js"
+  },
+  {
+    "revision": "8f948718793959f03372",
+    "url": "/swapi/static/css/main.e9461902.chunk.css"
+  },
+  {
+    "revision": "d9ab65a65cabcf4b1ce6",
+    "url": "/swapi/static/css/2.424fee09.chunk.css"
+  },
+  {
+    "revision": "24da07d6305bb59fc92c8ad1b3bf4666",
+    "url": "/swapi/index.html"
+  }
+];
